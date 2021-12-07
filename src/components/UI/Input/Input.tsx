f4dd35b4f13a6834a420d44fabf2022db538c272
Rasmus Fogelberg <fogelbergrasmus@gmail.com>
2021-12-07 16:43:44 +0100

    Add {...rest} to label in return

--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -2,11 +2,11 @@ import './input.css';
 
 const Input: React.FC<any & React.InputHTMLAttributes<HTMLInputElement>> = ({name, label, type, ...rest }) => {
   return (
-    <>
-      <label htmlFor={name}>{label}</label>
+    <> 
+      <label {...rest} htmlFor={name}>{label}</label><br />
       <input name={name} type={type} />
     </>
-  );
+  ); // Should {...rest} be in this one? It wasn't before but why is it called in the Const Input then?
 };
 
 export default Input;
