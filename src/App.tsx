c0174421fa333a8b6dbe2406fb513c7320abb899
Rasmus Fogelberg <fogelbergrasmus@gmail.com>
2021-12-14 18:16:46 +0100

    Update Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./App.css";
-import ArchivesPage from "./pages/Archive";
-import NewWorkoutPage from "./pages/NewWorkout";
-import MainNav from "./components/Navigation/MainNav";
+
+import ArchivesPage from "./pages/ArchivePage/ArchivePage";
+import NewWorkoutPage from "./pages/NewWorkoutPage/NewWorkoutPage";
 import ArchiveItemPage from "./pages/ArchiveItemPage/ArchiveItemPage";
+import NewWorkoutDetailPage from "./pages/NewWorkoutDetailPage/NewWorkoutDetailPage";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <MainNav />
-          <h1>Project GetFit</h1>
-          <Routes>
-            <Route path="/archive" element={<ArchivesPage />} />
-            <Route path="/archive/:workoutId" element={<ArchiveItemPage />} />
-            <Route path="/" element={<NewWorkoutPage />} />
-          </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/archive" element={<ArchivesPage />} />
+        <Route path="/archive/:workoutId" element={<ArchiveItemPage />} />
+        <Route path="/" element={<NewWorkoutPage />} />
+        <Route path="/new/:workoutId" element={<NewWorkoutDetailPage />} />
+      </Routes>
+    </Router>
   );
 }
 
