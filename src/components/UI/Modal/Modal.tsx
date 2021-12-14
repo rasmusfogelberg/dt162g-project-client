def294cac31e6fcf045bee9231e859e6f7259323
Rasmus Fogelberg <fogelbergrasmus@gmail.com>
2021-12-14 18:05:56 +0100

    Add Modal when creating exercise

---
+++ src/components/UI/Modal/Modal.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import './modal.css';
+
+import Button from "../Button/Button";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+
+interface ModalProps {
+  opened: boolean;
+  close: () => void;
+  children: React.ReactNode | React.ReactNode[];
+}
+
+const Modal: React.FC<ModalProps> = ({ opened, close, children }) => {
+  const modalClassName = (`${opened ? 'open' : 'closed'}`);
+
+  return ReactDOM.createPortal(
+    <>
+      <div className={`modalWrapper ${modalClassName}`}>
+        <Button className="modalCloseButton" style={{ color: 'white' }} onClick={close}>
+          <FontAwesomeIcon icon={solid("times")} color="white" />
+        </Button>
+        <div className="modalContent">
+          {children}
+        </div>
+      </div>
+    </>,
+    //@ts-ignore
+    document.getElementById("modal-root")
+  );
+};
+
+export default Modal;
