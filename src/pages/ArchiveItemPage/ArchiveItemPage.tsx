424a753798f98a24301820725f539da5b1ae24ed
Rasmus Fogelberg <fogelbergrasmus@gmail.com>
2021-12-18 11:16:13 +0100

    Enable delete set from archived workout

--- src/pages/ArchiveItemPage/ArchiveItemPage.tsx
+++ src/pages/ArchiveItemPage/ArchiveItemPage.tsx
@@ -8,6 +8,7 @@ import DefaultLayout from "../../layouts/DefaultLayout";
 import Exercise, { IExercise } from "../../components/Exercise/Exercise";
 import { Button } from "../../components/UI";
 import { updateWorkout } from "../../services/updateWorkout";
+import { ISet } from "../../components/Set/Set";
 
 /*
  * "View" for a single workout when clicked in the archives
@@ -43,25 +44,54 @@ function ArchiveItemPage() {
       }
     };
     fetchData();
+ 
   }, [workoutId]);
 
   // OnClick to delete an Exercise from a Workout
   const handleDeleteExercise = (workout: any, exerciseIndex: number) => {
-    const theWorkout = { ...workout };
+    const newWorkout = { ...workout };
 
-    theWorkout.exercises = theWorkout.exercises.filter(
+    newWorkout.exercises = newWorkout.exercises.filter(
       (_exercise: IExercise, index: number) => exerciseIndex !== index
     );
 
-    setWorkout(theWorkout);
+    setWorkout(newWorkout);
+  };
 
+  // OnClick to delete a single Set from an Exercise
+  const handleDeleteSet = (
+    workoutExercise: IExercise,
+    exerciseIndex: number,
+    setIndex: number
+  ) => {
+    const newWorkout = { ...workout };
+    const newExercise = { ...workoutExercise };
+
+    newExercise.sets = newExercise.sets.filter(
+      (_set: ISet, index: number) => setIndex !== index
+    );
+
+    const newExercises = [...workout.exercises]; // Copy all the exercises in state
+
+    // Replace our modified new exercise (which has the set removed)
+    // and replace the exercise at the index
+
+    newExercises[exerciseIndex] = newExercise;
+
+    newWorkout.exercises = newExercises;
+
+    setWorkout(newWorkout);
   };
 
   const handleUpdateWorkout = (workoutId: string) => {
-    const currentExercises = workout.exercises
+    const currentExercises = workout.exercises;
     updateWorkout(workoutId, currentExercises);
   };
 
+  const theClog = () => {
+    console.log(workout);
+
+  }
   // JSX code for the layout of the page
   return (
     <DefaultLayout>
@@ -73,7 +103,6 @@ function ArchiveItemPage() {
               <FontAwesomeIcon
                 icon={solid("pencil-alt")}
                 style={{ alignSelf: "center", cursor: "pointer" }}
-
                 onClick={() => setLocked(!locked)}
               />
             </h2>
@@ -89,20 +118,31 @@ function ArchiveItemPage() {
                   onDeleteExercise={() =>
                     handleDeleteExercise(workout, exerciseIndex)
                   }
+                  /* OnClick to delete the Set */
+                  onDeleteSet={(workoutExercise, exerciseIndex, setIndex) =>
+                    handleDeleteSet(workoutExercise, exerciseIndex, setIndex)
+                  }
                 />
               )
             )}
           </div>
           {workoutId && (
-                /* Button to save all the changes in the Workout */
-                <Button
-                  onClick={() => {
-                    handleUpdateWorkout(workoutId);
-                  }}
-                >
-                  Save Workout
-                </Button>
-              )}
+            /* Button to save all the changes in the Workout */
+            <Button
+              onClick={() => {
+                handleUpdateWorkout(workoutId);
+              }}
+            >
+              Save Workout
+            </Button>
+          )}
+          <Button
+            onClick={() => {
+              theClog();
+            }}
+          >
+            Test Clicker
+          </Button>
         </>
       )}
     </DefaultLayout>
